d120ec1d4d10ad738d78951f12f99b04929d47ca
hinicholasfu-cmyk <hinicholasfu@gmail.com>
2026-06-19 20:06:13 -0500

    add tabs

--- main.py
+++ main.py
@@ -3,3 +3,18 @@ import streamlit as st
 st.title("Welcome to me web page! 🥳🥳🥳")
 st.header("This is my blog")
 st.subheader("This is the first post")
+
+st.divider()
+
+st.markdown(
+'''
+My name is Nicholas, and I am a student
+
+'''
+)
+
+if st.button("Send balloons!"):
+  st.balloons()
+
+if st.button("Send snowflakes!"):
+  st.snow()
